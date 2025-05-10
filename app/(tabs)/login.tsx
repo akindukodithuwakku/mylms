b0e3059975bfbfa82cd5c4825fdf96ab87c9edd0
Akindu Kodithuwakku <akindukodithuwakku@gmail.com>
2025-05-10 22:45:41 +0530

    login basics

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,49 @@
+import { Link } from "expo-router";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+      />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+      <Button title="Login" onPress={() => {}} />
+      <Link href="/signup" style={styles.link}>
+        Don't have an account? Sign Up
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  input: {
+    width: "100%",
+    padding: 12,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    backgroundColor: "#fff",
+  },
+  link: {
+    marginTop: 16,
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+});
